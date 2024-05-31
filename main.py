99ce506c1144cd9bb3986cd6e31f55cb7ba124cd
Dev Makwana <devmakwana7704@gmail.com>
2024-05-31 12:07:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ st.write('Hello World')
 x = st.text_input('Favourite movie?')
 st.write(f'Your favourite movie is {x}')
 # st.link_button('Chart', url='/pages/2_charts.py')
+
+st.subheader(body='Demonstration of text writing in Streamlit', divider='violet')
+md = st.text_area(label='Type in your markdown string (without quotes)',
+                  value='*Happy* :rainbow[Streamlit-ing] :balloon:'
+                )
+
+st.code(f"""
+import streamlit as st
+st.markdown('''{md}''')
+""")
+
+st.markdown(md)
