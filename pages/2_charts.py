5f3d446c1e3de3b4012566ec87192b0175e0f0bc
Dev Makwana <devmakwana7704@gmail.com>
2024-05-31 12:08:35 +0530

    Update 2_charts.py

--- pages/2_charts.py
+++ pages/2_charts.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+samples = st.number_input(label='Enter no. of samples:',min_value=1,value=20)
+
 chart_data = pd.DataFrame(
-    np.random.randn(20,3),
+    np.random.randn(samples,3),
     columns = ['a', 'b', 'c']
 )
 
