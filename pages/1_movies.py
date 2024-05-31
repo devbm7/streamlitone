2b099081caf2ea14982b0a627e8ae66576ee6ae8
Dev Makwana <21cei036@nirmauni.ac.in>
2024-05-31 12:09:09 +0530

    Update 1_movies.py

--- pages/1_movies.py
+++ pages/1_movies.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+st.title('Movies Dataset')
+
+st.header('A _look_ at the dataset of :blue[movies]',divider='rainbow')
+st.markdown('*Source* is Kaggle, data from :red[rotten tomatoes]')
 data = pd.read_csv('movies.csv')
 st.write(data)
